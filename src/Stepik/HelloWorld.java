1b39692191f99bf23f493a5d567048a8d8ef52dc
Igor Povalyaev <zyx1275@gmail.com>
2022-12-24 13:18:29 +0300

    Stepik. Lesson1. HelloWorld

---
+++ src/Stepik/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * A program that prints <code>Hello world!</code>
+ *
+ * @author Igor Povalyaev
+ * @version 1.0
+ */
+package Stepik;
+public class HelloWorld {
+    /**
+     * Program entry point
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // вернуть не нулевой код возврата
+        // System.exit(-1);
+    }
+}
